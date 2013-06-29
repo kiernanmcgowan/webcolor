cbedf960abeecd5ff40b394502445aa91d1c0774
Tim McGowan <tim@vidmaker.com>
2013-06-29 17:17:29 -0500

    First pass of scraping and site abilities

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
+var config = require('../config/config.json');
+var pg = require('pg');
 
-/*
- * GET home page.
- */
+var client = new pg.Client(config.db);
+client.connect(function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('postgres started');
+   }
+});
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  client.query('SELECT * FROM site LIMIT 250', function(err, rs) {
+    res.render('index', {title: 'Internet color', data: rs.rows});
+  });
 };
--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,1 +1,89 @@
 # scrape.py
+
+# system
+from urlparse import urlparse
+import commands
+import subprocess
+import os
+import csv
+
+# db stuff
+import urllib
+import psycopg2 as db
+conn = db.connect(dbname='webcolor', user=os.environ['user'], host='localhost', password=os.environ['pass'])
+cursor = conn.cursor()
+
+# regex for color
+import re
+regex = re.compile("#[0-9a-fA-F]+")
+
+# grabs a url and creates an image of it
+def createImageOfURL(url, imgLocation):
+    try:
+        generateImage = 'gnome-web-photo -m photo --file ' + url + ' ' + imgLocation
+        status = commands.getstatusoutput(generateImage)
+        return True
+    except Exception as err:
+        print err
+        return False
+
+
+# extracts the top 5 colors of an image
+def extractPallet(img):
+    getPallet = 'convert ' + img + ' +dither -colors 5 -format "%c" histogram:info:'
+    status = commands.getstatusoutput(getPallet)
+    colors = regex.findall(status[1])
+    tmpCount = re.findall('\d+:', status[1])
+    count = []
+    for c in tmpCount:
+        count.append(int(c.replace(':', '')))
+
+    z = zip(count, colors)
+    z.sort(reverse=True)
+
+    return zip(*z)
+
+
+def storeSite(name, colors, count):
+    try:
+        cursor.execute('INSERT INTO site (hostname, colors, count) VALUES(%s, %s, %s)', (name, list(colors), list(count)))
+        conn.commit()
+        return True
+    except Exception as err:
+        print err
+        conn.rollback()
+        return False
+
+
+def cleanUp(img):
+    os.unlink(img)
+
+
+def getPalletForSite(hostname):
+    # may not work all the time, but good enough
+    url = 'http://' + hostname
+    imageLocation = './' + hostname + '.png'
+    print hostname
+    try:
+        if createImageOfURL(url, imageLocation):
+            pallet = extractPallet(imageLocation)
+            storeSite(hostname, pallet[1], pallet[0])
+            cleanUp(imageLocation)
+        else:
+            print 'oh no! it failed'
+    except Exception as err:
+        print err
+
+
+def processFile(srcCSV, maxCount):
+    with open(srcCSV, 'rb') as csvfile:
+        sites = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in sites:
+            if int(row[0]) > maxCount:
+                print 'hit top num - breaking'
+                break
+            getPalletForSite(row[1])
+
+
+processFile('../data/sites.csv', 500)
+#getPalletForSite('facebook.com')
